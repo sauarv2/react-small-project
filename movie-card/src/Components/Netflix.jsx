c3d4590a623a81ab48539bbb477bf8182d23e79e
saurav saha <sauravsaha024@gmail.com>
2025-05-20 22:15:08 +0530

    api project complete

--- movie-card/src/Components/Netflix.jsx
+++ movie-card/src/Components/Netflix.jsx
@@ -4,34 +4,38 @@ const Netflix = () => {
   return (
     <>
       <ul>
-        <div class="movie_card" id="bright">
-          <div class="info_section">
-            <div class="movie_header">
-              <img
-                class="locandina"
-                // src="https://movieplayer.net-cdn.it/t/images/2017/12/20/bright_jpg_191x283_crop_q85.jpg"
-                src={SerisData[0].img_url}
-              />
-              <h1>{SerisData[0].name}</h1>
-              <h4>2017, David Ayer</h4>
-              <span class="minutes">117 min</span>
-              <span class="minutes">Rating {SerisData[0].rating}</span>
-              <p class="type">{SerisData[0].genre}</p>
-            </div>
-            <div class="movie_desc">
-              <p class="text">{SerisData[0].description}</p>
-            </div>
-            <div class="movie_social">
-              <button
-                type="button "
-                className="inline-flex items-center border rounded-md border-transparent bg-red"
-              >
-                Watch now
-              </button>
-            </div>
-          </div>
-          <div class="blur_back bright_back"></div>
-        </div>
+        {SerisData.map((data) => {
+          return (
+            <li key={data.id}>
+              <div className="movie_card" id="bright">
+                <div className="info_section">
+                  <div className="movie_header ">
+                    <img
+                      className="locandina"
+                      // src="https://movieplayer.net-cdn.it/t/images/2017/12/20/bright_jpg_191x283_crop_q85.jpg"
+                      src={data.img_url}
+                    />
+                    <h1>{data.name}</h1>
+                    <h4>2017, David Ayer</h4>
+                    <span className="minutes">117 min</span>
+                    <span className="minutes">Rating {data.rating}</span>
+                    <p className="type">{data.genre}</p>
+                    <p className="type">Cast:{data.cast}</p>
+                  </div>
+                  <div className="movie_desc ">
+                    <p className="text">{data.description}</p>
+                  </div>
+                  <a href={data.watch_url} target="_blank">
+                    <button className="btn bg-red-600 text-white  items-center p-3 mt-50 text-2xl block rounded-2xl">
+                      Watch now
+                    </button>
+                  </a>
+                </div>
+                <div className="blur_back bright_back"></div>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
